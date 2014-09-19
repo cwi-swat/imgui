0a0e31b220fb6371f040ff98fc41decf14184e90
Tijs van der Storm <storm@cwi.nl>
2014-09-19 22:09:45 +0200

    Callstack must be key, also ids.

--- js/imgui3.js
+++ js/imgui3.js
@@ -73,8 +73,52 @@ function clone(obj) {
 }
 
 var memo = {};
+/*
+
+- we need the stack because of nesting
+def f()
+  g()
+  g()
+end
+
+def g()
+  h() > all h's will share viewstate even though they come via different g's
+end
+
+- view-state ~=~ with presentation structure ~=~ how the code unfolds
+- we need __LINE__ to deal with duplicate calls to same thing
+- we need model items to deal with for
+- a (minor?) problem
+if (x)
+  f(m, true)
+else
+  f(m, false)
+although you could say the intent is to share view-state, they won't be
+because different lines (could rewrite in this case to f(m, x), but is
+that general?
+
+Idea: visualize components as nested boxes + associated state.
+
+
+Paper
+- imgui: guis = output only, like drawing/printing
+- mutable
+- view-state maintenance: call traces + object ids + memoization (?)
+  - render different things of same type -> different view states
+  - render same thing multiple times (why we need loc)
+
+- immutable: render functions compute new values (models = algebraic data types)
+- paths for identity: view state update
+- application logic: compute new value
+- align view-state patch paths in memo table.
+
+*/
+
+
+var callStack = [];
 function callit(site, func, model, args, initViewState) {
-    var key = [site, func.name, objectId(model)].toString();
+    callStack.push([site, func.name, objectId(model)].toString());
+    var key = callStack.toString();
     console.log("Key = " + key);
     if (!memo[key]) {
         memo[key] = clone(initViewState);
@@ -82,19 +126,23 @@ function callit(site, func, model, args, initViewState) {
     }
     var mval = memo[key];
     var newArgs = [model].concat(args).concat([mval]);
-    return func.apply(this, newArgs);
+    var x = func.apply(this, newArgs);
+    callStack.pop();
+    return x;
 }
+
+// TODO: invent ids automatically.
 
 function todoView(item, idx, items, vs) {
     label("lab" + idx, item.label); 
-    for (var ev of checkBox("chk" + idx, item.done)) {
+    for (var ev of checkBox("chk" + callStack.toString(), item.done)) {
         if (ev)
             item.done = GUI.checked;
     }
-    for (var _ of button("del" + idx, "Delete")) 
+    for (var _ of button("del" + callStack.toString(), "Delete")) 
         items.splice(idx, 1);
 
-    for (var _ of button("vs" + idx, "Toggle viewstate"))  {
+    for (var _ of button("vs" + callStack.toString(), "Toggle viewstate"))  {
         vs.toggle = !vs.toggle;
     }
     
@@ -102,25 +150,36 @@ function todoView(item, idx, items, vs) {
  
 }
 
-function todoApp(todo, vs) {
+function todoList(todo, vs) {
     for (var ulEvent of ul("todos")) {
         for (var idx in todo.items) {
             for (var liEvent of li())
                 callit(__LINE__, todoView, todo.items[idx], [idx, todo.items], {toggle: false});
         }
-        for (var _ of li()) {
-            for (var _ of button("addit", "Add")) {
-                todo.items.push({label: vs.newTodo, done: false});
-                vs.newTodo = "";
-            }
-            for (var newTodo of textbox("new", vs.newTodo)) 
-                vs.newTodo = newTodo;
-        }
     }
+}
 
+function todoApp(todo, vs) {
+    callit(__LINE__, todoList, todo, [], {});
+
+    callit(__LINE__, todoList, todo, [], {});
+ 
+    br();
     
+    for (var _ of button("addit", "Add")) {
+        todo.items.push({label: vs.newTodo, done: false});
+        //vs.newTodo = "";
+    }
+    for (var newTodo of textbox("new", vs.newTodo)) 
+        vs.newTodo = newTodo;
+
+
+    br();
+    label("A", "Model: ");
     label("json", JSON.stringify(todo));
-    
+    br();
+    label("B", "View state: ");
+    label("viewstate", JSON.stringify(memo));
 
 }
 
