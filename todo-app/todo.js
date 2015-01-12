8721870d6b7b87ba03eab497c291958d3ee55f40
Tijs van der Storm <storm@cwi.nl>
2015-01-12 12:42:06 +0100

    Cleanup of todo app.

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -5,7 +5,10 @@ var imgui = require('../libimgui');
 var todos = {
     items: [
         {label: "Email", done: false},
-        {label: "Reviewing", done: true}
+        {label: "Reviewing", done: true},
+        {label: "Buy milk", done: false},
+        {label: "Send postcard", done: true},
+        {label: "Finish annual report", done: false}
     ]
 };
 
@@ -17,22 +20,26 @@ function run() {
 var todoApp = imgui.component({newTodo: ""}, function todoApp(self, model) {
 
     imgui.h2("Todo App");
+
+
+    function todoView(item) {
+	item.done = imgui.checkBox(item.done);
+	item.label = editableLabel(item.label);
+    }
     
-    imgui.named("todos1", editableList, model.items, todoView, {done: false, label: ""});
-    imgui.named("todos2", editableList, model.items, todoView, {done: false, label: ""});
+    
+    var newTodo = {done: false, label: "New todo"};
+    editableList(model.items, todoView, newTodo);
     
     if (imgui.button("Add")) {
         model.items.push({label: self.newTodo, done: false});
         self.newTodo = "";
     }
     
-    self.newTodo = imgui.textbox(self.newTodo);
-    
-    // imgui.h3("The model");
-    // editableValue(model);
+    self.newTodo = imgui.textBox(self.newTodo);
 
-    // imgui.h3("The memo table containing viewstates");
-    // editableValue(imgui.memo);
+    imgui.p(JSON.stringify(imgui.memo));
+    
 });
 
 
@@ -40,40 +47,39 @@ var todoApp = imgui.component({newTodo: ""}, function todoApp(self, model) {
 
 var editableLabel = imgui.component({editing: false}, function editableLabel(self, txt) {
     var result = txt;
+
     function setFocus(elt) {
 	elt.focus();
     }
     
     if (self.editing) {
-	result = imgui.textbox(txt, {extra: setFocus});
-	self.editing = false;
+	imgui.on("input", ["blur"], {type: "text", value: txt, extra: setFocus}, function (ev) {
+	    if (ev) {
+		self.editing = false;
+		result = ev.target.value;
+	    }
+	});
     }
     else {
 	imgui.on("span", ["dblclick"], {}, function (ev) {
-	    if (ev) self.editing = true;
+	    if (ev) {
+		self.editing = true;
+	    }
 	    imgui.text(txt);
 	});
     }
+
     return result;
 });
 
 
-var todoView = imgui.component({toggle: false}, function todoView(self, item) {
-    item.label = editableLabel(item.label);
-    
-    item.done = imgui.checkbox(item.done);
-    if (imgui.button("Toggle viewstate"))  {
-        self.toggle = !self.toggle;
-    }
-    
-    imgui.text(self.toggle);
- 
-});
-
-
-
 
 var editableList = imgui.component({}, function editableList(self, xs, renderx, newx) {
+
+    if (xs.length == 0 && imgui.button(" + ")) {
+	xs[0] = imgui.clone(newx);
+	return xs;
+    }
 
     function move(idx, dir) {
 	var elt = xs[idx];
@@ -81,31 +87,41 @@ var editableList = imgui.component({}, function editableList(self, xs, renderx, 
         xs.splice(idx + dir, 0, elt);
     }
 
-    imgui.ul(function () {
-        if (xs.length == 0 && imgui.button(" + ")) {
-	    xs[0] = imgui.clone(newx);
-        }
+    
+    imgui.table(function () {
 
 	// iterate over a copy
 	var elts = xs.slice(0);
 	
         for (var idx = 0; idx < elts.length; idx++) {
+	    imgui.tr(function() {
+		imgui.td(function () {
+                    renderx(elts[idx]);
+		});
 
-            imgui.li(".completed", function () {
-                renderx(elts[idx]);
+		imgui.td(function() {
+                    if (imgui.button(" + ")) {
+			xs.splice(idx + 1, 0, imgui.clone(newx));
+                    }
+		});
+		
+                imgui.td(function() {
+		    if (imgui.button(" - ")) {
+			xs.splice(idx, 1);
+                    }
+		});
 
-                if (imgui.button(" + ")) {
-		    xs.splice(idx + 1, 0, imgui.clone(newx));
-                }
-                if (imgui.button(" - ")) {
-		    xs.splice(idx, 1);
-                }
-                if (idx > 0 && imgui.button(" ^ ")) {
-		    move(idx, -1);
-                }
-                if (idx < xs.length - 1 && imgui.button(" v ")) {
-		    move(idx, +1);
-                }
+		imgui.td(function() {
+                    if (idx > 0 && imgui.button(" ^ ")) {
+			move(idx, -1);
+                    }
+		});
+
+		imgui.td(function() {
+                    if (idx < xs.length - 1 && imgui.button(" v ")) {
+			move(idx, +1);
+                    }
+		});
             });
 	    
         }
